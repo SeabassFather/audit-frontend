8a7366a72659059fea6bf392d95308abf58bb101
YourGitHubUsername <your@email.com>
2025-09-29 12:35:48 -0700

    Add AuditDNA full 300+ services (split JSON by category)

---
+++ src/lib/services/index.js
@@ -0,0 +1,17 @@
+﻿import agriculture from "./agriculture.json";
+import compliance from "./compliance.json";
+import eco from "./eco.json";
+import finance from "./finance.json";
+import legal from "./legal.json";
+import consumer from "./consumer.json";
+
+const services = [
+  ...agriculture,
+  ...compliance,
+  ...eco,
+  ...finance,
+  ...legal,
+  ...consumer
+];
+
+export default services;
