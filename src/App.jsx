b5a11ac29a5fb15c0cabbd934665e5ae6f3a069c
YourGitHubUsername <your@email.com>
2025-09-29 13:13:02 -0700

    Add Services page with Accordion directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Dashboard from "./pages/Dashboard";
-import USDA from "./pages/USDA";
-import Mortgage from "./pages/Mortgage";
-import Factoring from "./pages/Factoring";
-import Compliance from "./pages/Compliance";
-import WaterTech from "./pages/WaterTech";
-import AuditReport from "./pages/AuditReport";
+import ServicesPage from "./pages/Services";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/usda" element={<USDA />} />
-          <Route path="/mortgage" element={<Mortgage />} />
-          <Route path="/factoring" element={<Factoring />} />
-          <Route path="/compliance" element={<Compliance />} />
-          <Route path="/water-tech" element={<WaterTech />} />
-          <Route path="/audit-report" element={<AuditReport />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
-}
+// inside <nav> links array:
+["/services", "Services"],
+
+// inside <Routes>:
+<Route path="/services" element={<ServicesPage />} />
