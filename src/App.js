6c24a298ffa73d0553b22761edd74620aefb8854
YOUR_GITHUB_USERNAME <YOUR_EMAIL>
2025-10-29 18:30:14 -0700

    fix(app): remove merge markers and restore valid src/App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+﻿import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/Layout";
-import Dashboard from "./pages/Dashboard";
-import TradePortal from "./pages/TradePortal";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/trade-portal" element={<TradePortal />} />
-        </Routes>
-      </Layout>
+      <Layout />
     </Router>
   );
 }
+
 export default App;
