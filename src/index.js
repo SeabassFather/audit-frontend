69b5f8c95feba04addfaee7798f8b26c02b9baaa
YOUR_GITHUB_USERNAME <YOUR_EMAIL>
2025-10-29 18:27:33 -0700

    fix(index): remove merge markers and restore valid src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+﻿import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// If you have CSS or index.scss, import it here (uncomment if needed)
+// import "./index.css";
+
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  throw new Error(
+    'Root element not found: add <div id="root"></div> to your index.html'
+  );
+}
+
+const root = ReactDOM.createRoot(rootEl);
 root.render(
   <React.StrictMode>
     <App />
