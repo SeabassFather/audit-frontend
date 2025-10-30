591c5e2a41e5b9545ae74c94db192ff929770c24
YOUR_GITHUB_USERNAME <YOUR_EMAIL>
2025-10-29 18:32:33 -0700

    fix(footer): remove merge markers and restore Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,13 @@
-import React from "react";
+﻿import React from "react";
 
-const Footer = () => (
-  <footer>
-    <div>
-      © 2024 AuditDNA. All rights reserved. | Licensed Financial Services Provider
-    </div>
-    <div>
-      NMLS ID: 123456 | Equal Housing Opportunity
-    </div>
-  </footer>
-);
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        © 2024 AuditDNA. All rights reserved. | Licensed Financial Services Provider
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
